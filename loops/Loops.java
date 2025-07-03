520dd4939c9c7a773fe9cc6a1053ce2709a7caaf
victorliraetc <104510669+victorlirafront@users.noreply.github.com>
2025-07-02 22:34:51 -0300

    feat: add callDoWhileLoop function

--- loops/Loops.java
+++ loops/Loops.java
@@ -1,17 +1,41 @@
 public class Loops {
     public static void main(String[] args){
-        int forLoopResult = callForLoop();
+        int callForLoopResult = callForLoop();
+        int callWhileLoopResult = callWhileLoop();
+        int callDoWhileLoopResult = callDoWhileLoop();
 
-        System.out.println(forLoopResult);
+        System.out.println("forLoopResult " + callForLoopResult);
+        System.out.println("callWhileLoop " + callWhileLoopResult);
+        System.out.println("callDoWhileLoop " + callDoWhileLoopResult);
+    }
+
+    public static int callDoWhileLoop(){
+        int result = 0;
+        int i = 0;
+
+        do {
+            result += i;
+            i++;
+        }while (i < 5);
+
+        return result;
     }
 
     public static int callForLoop(){
         int result = 0;
-
         for(int i = 0; i < 10; i++) {
             result += i;
         }
+        return result;
+    }
 
+    public static int callWhileLoop() {
+        int i = 0;
+        int result = 0;
+        while (i < 5){
+            result += i;
+            i++;
+        }
         return result;
     }
 }
