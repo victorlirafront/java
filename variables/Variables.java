a2b84584ebf0cad87b90d66122dc59a83d47de0e
victorliraetc <104510669+victorlirafront@users.noreply.github.com>
2025-07-02 21:46:09 -0300

    refactor: update Variables file

--- variables/Variables.java
+++ variables/Variables.java
@@ -1,5 +1,13 @@
 public class Variables {
     public static void main(String[] args){
-        System.out.println("Call variables");
+        String name = "Victor";
+        int age = 28;
+        boolean isStudent = true;
+        double height = 1.75;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isStudent);
+        System.out.println(height);
     }
 }
