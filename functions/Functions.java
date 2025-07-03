93b694e6f0beadc5c8274f3a13dafc0899458fa6
victorliraetc <104510669+victorlirafront@users.noreply.github.com>
2025-07-03 12:42:16 -0300

    feat: add callTeachers function

--- functions/Functions.java
+++ functions/Functions.java
@@ -3,10 +3,19 @@ public class Functions {
         boolean isEvenResult = isEven(2);
         boolean isUnderAgeResult = isUnderAge(15);
         int numberOfStudentsResult = numberOfStudents(new String[]{"Mark", "Sarah", "Victor"});
+        String[] callTeachersResult = callTeachers();
 
         System.out.println("Is this an even number ? " + isEvenResult);
         System.out.println("Is mark under age ? " + isUnderAgeResult);
         System.out.println("Number of students: " + numberOfStudentsResult);
+        for (String teacher : callTeachersResult) {
+            System.out.println("Teacher: " + teacher);
+        }
+    }
+
+    public static String[] callTeachers (){
+        String[] teachers = new String[]{"Verônica", "Claudia", "André"};
+        return teachers;
     }
 
     public static boolean isEven(int number){
