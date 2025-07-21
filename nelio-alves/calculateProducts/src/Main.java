744788f43d338f5a73c2efbd485e5099d5a47a8a
victorlirafront <victorliradeveloper@gmail.com>
2025-07-21 07:54:17 -0300

    feat: add new data entry

--- nelio-alves/calculateProducts/src/Main.java
+++ nelio-alves/calculateProducts/src/Main.java
@@ -21,7 +21,25 @@ public class Main {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product.toString());
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
 
         sc.close();
     }
