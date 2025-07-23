69fecb7226b97aee837202db08bd278fd33793d6
victorlirafront <victorliradeveloper@gmail.com>
2025-07-23 07:37:16 -0300

    feat: implement example of encapsulation in Java

--- nelio-alves/calculateProducts/src/application/entities/Product.java
+++ nelio-alves/calculateProducts/src/application/entities/Product.java
@@ -1,9 +1,9 @@
 package application.entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity){
         this.name = name;
@@ -21,6 +21,26 @@ public class Product {
 
     public void removeProducts(int quantity){
         this.quantity -= quantity;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+         return price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
     }
 
     public String toString() {
