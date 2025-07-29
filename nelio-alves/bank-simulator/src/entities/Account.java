331f3f8dbbd8954f4e816301a02101340585f825
victorlirafront <victorliradeveloper@gmail.com>
2025-07-29 08:59:53 -0300

    feat: add account number

---
+++ nelio-alves/bank-simulator/src/entities/Account.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Account {
+    private int number;
+    private String holder;
+    private double balance;
+
+    public  Account (int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    public  Account (int number, String holder, double initialDeposit) {
+        this.number = number;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+    public String getHolder(){
+        return this.holder;
+    }
+
+    public String setHolder(int number){
+        return this.holder;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    public int getBalance(){
+        return this.number;
+    }
+
+    public void deposit (double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5.0;
+    }
+}
